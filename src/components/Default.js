4ecc96f8114abaf0a0cdcac30b818a8d9b645d87
George Tang <georgecctang@gmail.com>
2020-12-28 17:22:04 -0500

    create default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,6 +1,15 @@
-export default function Default() {
+export default function Default(props) {
   return (
-    <h3>page not found..</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+          <h1 className="display-3">404</h1>
+          <h1>Error</h1>
+          <h2>page not found</h2>
+          <h3>the requested URL <span className="text-danger">{props.location.pathname}</span> was not found</h3>
+        </div>
+      </div>
+    </div>
 
   )
 
